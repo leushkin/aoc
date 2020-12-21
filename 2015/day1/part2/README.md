917bfe15c417070d8c2ab2e8c8beee45d91efda9
leushkin <kirill.leushkin1@gmail.com>
2020-12-21 22:57:30 +0300

    [AOC-2015] day1/part2 (OCaml)

---
+++ 2015/day1/part2/README.md
@@ -0,0 +1,6 @@
+# [Day 1](https://adventofcode.com/2015/day/1)
+
+## Quick start
+
+- make sure that you have installed corebuild
+- `$ make`
---
+++ 2015/day1/part2/main.ml
@@ -0,0 +1,41 @@
+open Core
+open Condition
+
+exception SomethingWentWrong
+
+let char_list_of_string s = List.init (String.length s) (String.get s)
+
+let read_file (file_path : string) : string =
+  let ch = open_in file_path in
+  let n = in_channel_length ch in
+  let s = really_input_string ch n in
+  close_in ch;
+  s
+
+let helper = function
+  | Some x -> (
+      match x with
+      | '(' -> 1
+      | ')' -> -1
+      | _ -> raise SomethingWentWrong
+    )
+  | None -> raise SomethingWentWrong
+
+let solution (str: char list) : int =
+  let i = ref 0 in
+  let acc = ref 0 in
+  while !i != -1 do
+    acc := !acc + (!i |> List.nth str |> helper);
+    i := !i + 1
+  done;
+  !i
+
+let () =
+  let args = () |> Sys.get_argv in
+  if (=) (args |> Array.length) 2
+  then
+    let input = args.(1) |> read_file |> char_list_of_string in
+    solution input |> string_of_int |> print_string
+  else print_string "Usage: ./solution <file_name>"
+
+
